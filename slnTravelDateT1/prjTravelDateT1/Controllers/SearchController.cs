32d64462b33a95ca213f4301835f74923d5054ee
pengyazhi <yzyz86031614@gmail.com>
2023-08-01 21:04:07 +0800

    0801mutiRange

--- slnTravelDateT1/prjTravelDateT1/Controllers/SearchController.cs
+++ slnTravelDateT1/prjTravelDateT1/Controllers/SearchController.cs
@@ -3,26 +3,26 @@ using prjTravelDateT1.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 
 namespace prjTravelDateT1.Controllers
 {
     public class SearchController : Controller
     {
+        
         // GET: Search
         public ActionResult SearchList()
         {
             CFilteredProductFactory products = new CFilteredProductFactory();
             CSearchListViewModel vm = new CSearchListViewModel();
-            //商品cards
-            vm.filterProducts = products.qureyFilterProductsInfo(); ;
-            //商品類別&標籤,左邊篩選列
-            vm.categoryAndTags = products.qureyFilterCategories();
-            //商品國家&縣市,左邊篩選列
-            vm.countryAndCities = products.qureyFilterCountry(); 
-            //商品類型,左邊篩選列
-            vm.types = products.qureyFilterTypes();
+            vm.filterProducts = products.qureyFilterProductsInfo();//商品cards
+            vm.categoryAndTags = products.qureyFilterCategories();//商品類別&標籤,左邊篩選列
+            vm.countryAndCities = products.qureyFilterCountry();  //商品國家&縣市,左邊篩選列
+            vm.types = products.qureyFilterTypes();//商品類型,左邊篩選列
+
             return View(vm);
         }
         
